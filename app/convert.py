4bc882a792b0614362b10f4cdc4aa8aa1e947803
André Jernung <forcie@gmx.net>
2024-09-09 07:31:20 +0200

    testdata to dict

--- app/convert.py
+++ app/convert.py
@@ -5,15 +5,17 @@ with open('testdata.csv', newline='') as f:
     reader = csv.reader(f)
     data = list(reader)
 
-    jsondicts = []
+    jsondicts = {}
+    index = -1
 
     for line in data:
+        index = index + 1
         newdict = {}
         newdict["id"] = line[0]
         newdict["name"] = line[1]
         newdict["account"] = line[2]
         newdict["org"] = line[3]
-        jsondicts.append(newdict)
+        jsondicts.update({index: newdict})
     
     with open("testdata.json", "w") as outfile:
         json.dump(jsondicts, outfile, indent=2)
